d85246e6e9c128c92ab4603373ce6e98d51a92e7
mikael <mikaeljuliao56@gmail.com>
2025-08-11 00:58:09 -0300

    finalizei a aula2

--- aula2/app.js
+++ aula2/app.js
@@ -1,1 +1,3 @@
-reactDOM.render(<h1>olá mundo do React</h1>,document.getElementById("root"))
+//BÁSICO DO REACT
+ReactDOM.render(<h1>Olá Mundo!</h1>, document.getElementById("root"));
+ReactDOM.render(<p>Isso é um paragrafo feito com React</p>, document.getElementById("root"))
--- aulas-react/app.js
+++
@@ -1,2 +0,0 @@
-//BÁSICO DO REACT
-ReactDOM.render(<h1>Olá Mundo!</h1>, document.getElementById("root"))
